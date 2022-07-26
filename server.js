2abfe74e4099f77b6637649f2e2dc2891d498e88
Andrew <andrewunifei@gmail.com>
2022-07-26 18:44:59 -0300

    Configurações de variáveis de ambiente

--- server.js
+++ server.js
@@ -5,22 +5,33 @@ const path = __dirname + "/dist/";
 const app = express()
 
 app.use(express.static(path))
-app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
+// Determina se é produção ou desenvolvimento
+if(process.env.NODE_ENV == 'production'){
+    app.use(cors());
 
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-});
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+            'Access-Control-Allow-Header',
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+    
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).send({});
+        }
+        next();
+    });
+}
+else{
+    var corsOptions = {
+        origin: "http://localhost:8081"
+    };
+
+    app.use(cors(corsOptions));
+}
 
 const db = require("./models");
 db.sequelize.sync();
