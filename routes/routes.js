ce002e9a2026b04c0587b0ac1655cc4b013d1bcf
Andrew <andrewunifei@gmail.com>
2022-07-26 18:16:07 -0300

    Backend

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const router = express.Router();
 const auth = require("./controllers/auth.controller.js");
 const associado = require("./controllers/associado.controller.js");
 const publicacao = require("./controllers/publicacao.controller.js");
@@ -7,27 +6,28 @@ const exemplar = require("./controllers/exemplar.controller.js");
 const reserva = require("./controllers/reserva.controller.js");
 const emprestimo = require("./controllers/emprestimo.controller.js");
 
-module.exports = () => {
-    // Funcionário
-    router.post("/", auth.login);
+const router = express.Router();
 
-    router.post("/cadastrar_func", auth.cadastrar_func);
+// Funcionário
+router.post("/", auth.login);
+router.post("/cadastrar_func", auth.cadastrar_func);
 
-    // Associado
-    router.post("/associado", associado.login);
-    router.post("/cadastrar-assoc", associado.cadastrar_assoc);
+// Associado
+router.post("/associado", associado.login);
+router.post("/cadastrar-assoc", associado.cadastrar_assoc);
 
-    // Publicação
-    router.post("/cadastrar-pub", publicacao.cadastrar_pub);
-    router.post("/consultar-pub", publicacao.consultar_pub);
+// Publicação
+router.post("/cadastrar-pub", publicacao.cadastrar_pub);
+router.post("/consultar-pub", publicacao.consultar_pub);
 
-    // Exemplar
-    router.post("/cadastrar-exemplar", exemplar.cadastrar_exemplar);
+// Exemplar
+router.post("/cadastrar-exemplar", exemplar.cadastrar_exemplar);
 
-    // Reserva
-    router.post("/cadastrar-reserva", reserva.cadastrar_reserva);
+// Reserva
+router.post("/cadastrar-reserva", reserva.cadastrar_reserva);
 
-    // Empréstimo
-    router.post("/cadastrar-emp", emprestimo.cadastrar_emprest);
-    router.post("/devolucao", emprestimo.devolucao);
-}
+// Empréstimo
+router.post("/cadastrar-emp", emprestimo.cadastrar_emprest);
+router.post("/devolucao", emprestimo.devolucao);
+
+module.exports = router
