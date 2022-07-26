475ebe263bae7f7fc529b8d22801ee217eb666bb
Andrew <andrewunifei@gmail.com>
2022-07-26 10:47:02 -0300

    Reconstrução

--- server.js
+++ server.js
@@ -1,34 +1,12 @@
-const express = require("express");
-const path = __dirname + "/dist/";
-// const bodyParser = require("body-parser"); /* deprecated */
-const cors = require("cors");
+const express = require("express")
 
-const app = express();
+const app = express()
 
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-
-// app.use(cors(corsOptions));
-
-app.use(express.json());  /* bodyParser.json() is deprecated */
-//app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
-app.use(express.static(path))
-
-const db = require("./app/models");
-db.sequelize.sync();
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome to bezkoder application." });
-// });
-
-app.get('/', (req, res) =>
-    res.sendFile(path + 'index.html'),
-);
-
-require("./app/routes/routes")(app);
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+app.get('/', (req, res) => {
+    res.send("Hello world!")
 });
+
+const PORT = process.env.PORT  || 8080;
+app.listen(PORT, () => {
+    console.log("Servidor ouvindo na porta", PORT)
+})
