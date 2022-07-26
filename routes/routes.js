44eb0b5a0e4c0055fd33f2eb4198b39669118817
Andrew <andrewunifei@gmail.com>
2022-07-26 18:12:14 -0300

    Backend

---
+++ routes/routes.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const router = express.Router();
+const auth = require("./controllers/auth.controller.js");
+const associado = require("./controllers/associado.controller.js");
+const publicacao = require("./controllers/publicacao.controller.js");
+const exemplar = require("./controllers/exemplar.controller.js");
+const reserva = require("./controllers/reserva.controller.js");
+const emprestimo = require("./controllers/emprestimo.controller.js");
+
+module.exports = () => {
+    // Funcionário
+    router.post("/", auth.login);
+
+    router.post("/cadastrar_func", auth.cadastrar_func);
+
+    // Associado
+    router.post("/associado", associado.login);
+    router.post("/cadastrar-assoc", associado.cadastrar_assoc);
+
+    // Publicação
+    router.post("/cadastrar-pub", publicacao.cadastrar_pub);
+    router.post("/consultar-pub", publicacao.consultar_pub);
+
+    // Exemplar
+    router.post("/cadastrar-exemplar", exemplar.cadastrar_exemplar);
+
+    // Reserva
+    router.post("/cadastrar-reserva", reserva.cadastrar_reserva);
+
+    // Empréstimo
+    router.post("/cadastrar-emp", emprestimo.cadastrar_emprest);
+    router.post("/devolucao", emprestimo.devolucao);
+}
--- server.js
+++ server.js
@@ -29,39 +29,8 @@ app.get('/', (req, res) => {
     res.sendFile(path + 'index.html')
 });
 
-const router = express.Router();
-const auth = require("./controllers/auth.controller.js");
-const associado = require("./controllers/associado.controller.js");
-const publicacao = require("./controllers/publicacao.controller.js");
-const exemplar = require("./controllers/exemplar.controller.js");
-const reserva = require("./controllers/reserva.controller.js");
-const emprestimo = require("./controllers/emprestimo.controller.js");
-
-// Funcionário
-router.post("/", auth.login);
-
-router.post("/cadastrar_func", auth.cadastrar_func);
-
-// Associado
-router.post("/associado", associado.login);
-router.post("/cadastrar-assoc", associado.cadastrar_assoc);
-
-// Publicação
-router.post("/cadastrar-pub", publicacao.cadastrar_pub);
-router.post("/consultar-pub", publicacao.consultar_pub);
-
-// Exemplar
-router.post("/cadastrar-exemplar", exemplar.cadastrar_exemplar);
-
-// Reserva
-router.post("/cadastrar-reserva", reserva.cadastrar_reserva);
-
-// Empréstimo
-router.post("/cadastrar-emp", emprestimo.cadastrar_emprest);
-router.post("/devolucao", emprestimo.devolucao);
-
-// appRouter = require("./routes/routes.js");
-app.use("/api", router);
+appRouter = require("./routes/routes.js");
+app.use("/api", appRouter);
 
 const PORT = process.env.PORT  || 8080;
 app.listen(PORT, () => {
