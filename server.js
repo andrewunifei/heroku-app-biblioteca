166514e2644bd56c0cdef5bb56daed1fa0b50a59
Andrew <andrewunifei@gmail.com>
2022-07-26 10:49:50 -0300

    Reconstrução

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hello world!")
+    res.send("Olá mundo!")
 });
 
 const PORT = process.env.PORT  || 8080;
