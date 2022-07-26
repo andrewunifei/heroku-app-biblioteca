b4117c2110776fffb219f10156b1b0fb1d2d05fd
Andrew <andrewunifei@gmail.com>
2022-07-26 12:51:35 -0300

    Reconstrução

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const app = express()
 app.use(express.static(path))
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+});
+
 const db = require("./models");
 db.sequelize.sync();
 
