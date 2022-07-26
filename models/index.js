049ed433dd2a3369f7dfef8d267d2bb0fa0c1826
Andrew <andrewunifei@gmail.com>
2022-07-26 18:07:17 -0300

    Segurança

--- models/index.js
+++ models/index.js
@@ -1,14 +1,31 @@
 const dbConfig = require("../config/db.config.js");
-
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+let sequelize;
+
+// Determina se é produção ou desenvolvimento
+if(process.env.NODE_ENV == 'production'){
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-  }
-});
+  });
+}
+else{
+  sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+  
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  });
+}
 
 const db = {};
 
